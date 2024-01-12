86d18bb708d8d46601fdb6d85716550cfe1454d7
tongWz <tongweizhen@foxmail.com>
2024-01-12 18:03:21 +0800

    xiguai

--- twz_mappings/normal.lua
+++ twz_mappings/normal.lua
@@ -63,4 +63,12 @@ return {
     function() require("astronvim.utils.buffer").nav(-(vim.v.count > 0 and vim.v.count or 1)) end,
     desc = "Previous buffer",
   },
+  -- 整行向下
+  ["<M-j>"] = {
+    "<cmd>:m .+1<CR>gv=gv",
+  },
+  -- 整行向上
+  ["<M-k>"] = {
+    "<cmd>:m .-2<CR>gv=gv",
+  },
 }
