da15ad55874649db23928d0722a51eca018af690
童伟珍 <tongweizhen@foxmail.com>
2024-01-28 10:58:15 +0800

    xiuguai

--- twz_func/golbal_func.lua
+++ twz_func/golbal_func.lua
@@ -5,16 +5,23 @@ local commentText = [[
  * @auth: tongWz
  * @date: $date
 **/]]
-local currentDate = os.date "%Y-%m-%d %H:%M:%S"
-local replacedComment = string.gsub(commentText, "%$date", currentDate)
 
 -- 在当前函数内 函数前面一行写入注释
 function insert_comment()
+  vim.api.nvim_command "set paste"
+  local currentDate = os.date "%Y-%m-%d %H:%M:%S"
+  local replacedComment = string.gsub(commentText, "%$date", currentDate)
+  -- :gsub("^%s*", "")
   vim.api.nvim_command("normal [{O" .. replacedComment)
   vim.api.nvim_command "normal kkk$"
+  vim.api.nvim_command "set nopaste"
 end
 -- 这个在当前行上一行写入注释
 function insert_comment2()
+  vim.api.nvim_command "set paste"
+  local currentDate = os.date "%Y-%m-%d %H:%M:%S"
+  local replacedComment = string.gsub(commentText, "%$date", currentDate)
   vim.api.nvim_command("normal O" .. replacedComment)
   vim.api.nvim_command "normal kkk$"
+  vim.api.nvim_command "set nopaste"
 end
