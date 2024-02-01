ce3da77a7290e567b0085f4c905bb230767911dd
tongWz <tongweizhen@foxmail.com>
2024-02-01 16:49:07 +0800

    xiugai

--- options.lua
+++ options.lua
@@ -10,10 +10,12 @@ return {
     foldcolumn = "2",
     tabstop = 4, -- 这个用来看一个tab是几个空格
     shiftwidth = 4,
+    scrolloff = 999, -- 设置光标始终在文档流中间位置-原理是将光标与顶部与底部最近距离设置到很大从而两边都有很大的最近距离
   },
   g = {
     mapleader = " ", -- sets vim.g.mapleader
-    autoformat_enabled = true, -- enable or disable auto formatting at start (lsp.formatting.format_on_save must be enabled)
+    -- 将保存后就格式化代码关掉因此每次都格式化会很影响保存代码的效率
+    autoformat_enabled = false, -- enable or disable auto formatting at start (lsp.formatting.format_on_save must be enabled)
     cmp_enabled = true, -- enable completion at start
     autopairs_enabled = true, -- enable autopairs at start
     diagnostics_mode = 3, -- set the visibility of diagnostics in the UI (0=off, 1=only show in status line, 2=virtual text off, 3=all on)
